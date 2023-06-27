b4d0097e5c0b1e578dc2d3aed043c6de531919d5
juan <juanarangozapata35@gmail.com>
2023-06-26 21:47:27 -0500

    carrusel añadido en Inicio

--- src/Screens/Inicio.jsx
+++ src/Screens/Inicio.jsx
@@ -1,31 +1,89 @@
-import { View, Text , Button, StyleSheet} from 'react-native'
-import React from 'react'
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {View, Text, Button, StyleSheet, Dimensions} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import Carousel from 'react-native-reanimated-carousel';
+
+import {Image} from 'react-native';
 
 const Inicio = ({navigation}) => {
+  const width = Dimensions.get('window').width;
+
+  const images = [
+    require('../assets/paisaje1.jpeg'),
+    require('../assets/paisaje2.jpg'),
+    require('../assets/paisaje3.jpeg'),
+    require('../assets/paisaje4.jpg'),
+    require('../assets/paisaje5.jpg'),
+    require('../assets/paisaje6.jpg'),
+  ];
+
   return (
-    <View style={styles.fondo} >
-      <Text style={styles.texto} >Esramos en Inicio</Text>
-      <Button title='Vamos a Perfil'  onPress={()=>navigation.navigate('Perfil')} />
-    </View>
-  )
-}
+    <GestureHandlerRootView style={{flex: 1}}>
+      {/*       caja principal */}
+      <View style={styles.fondo}>
+        <Text style={{fontSize: 30, fontWeight: 'bold', color: '#FB1F09', padding:20}}>
+          {' '}
+          Carrusel de Paisajes
+        </Text>
+        <Carousel
+          loop
+          width={width}
+          height={width / 2}
+          autoPlay={true}
+          data={images}
+          scrollAnimationDuration={2000}
+          renderItem={({item}) => (
+            <View style={{flex: 1, borderWith: 1, justifyContent: 'center'}}>
+              <Image
+                source={item}
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  borderRadius: 25,
+                  padding: 10,
+                  borderColor: 'white',
+                }}
+                resizeMode="cover"
+              />
+            </View>
+          )}
+        />
 
-export default Inicio; 
+        <View style={styles.textobajo} >
+          <Text style={styles.texto}>Estamos en Inicio</Text>
+          <Button
+            title="Vamos a Perfil"
+            onPress={() => navigation.navigate('Perfil')}
+          />
+        </View>
+      </View>
+    </GestureHandlerRootView>
+  );
+};
 
+export default Inicio;
 
 const styles = StyleSheet.create({
   fondo: {
-    flex:1,
+    flex: 1,
     justifyContent: 'center',
+
     alignItems: 'center',
-  
-    backgroundColor: '#2CF7DE'
-  }, 
+
+    backgroundColor: '#2CF7DE',
+  },
 
   texto: {
     fontSize: 30,
-    fontWeight: 'bold', 
-    padding: 20, 
-    color: 'blue'
+    fontWeight: 'bold',
+    padding: 20,
+    color: 'blue',
+  },
+  textobajo:{
+    flex: 0.5,
+    
+    justifyContent: 'flex-end'
+
   }
 });
